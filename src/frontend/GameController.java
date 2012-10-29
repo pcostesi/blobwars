f5e01dd0b1cdffe76629c191561d4ac132494a84
Kevin J. Hanna <kevinjackhanna@gmail.com>
2012-10-29 11:17:34 -0300

    Make computer play in graphics mode

--- src/frontend/GameController.java
+++ src/frontend/GameController.java
@@ -2,12 +2,14 @@ package frontend;
 
 import game.Game;
 import game.Movement;
+import structures.MinMaxTree;
 import structures.Point;
 
 public class GameController{
 
 	private Game game;
 	private Window container;
+	boolean humanTurn;
 
 	public GameController() {
 		initialize();
@@ -16,14 +18,20 @@ public class GameController{
 	private void initialize() {
 		container = new Window(this);
 		game = new Game();
+		humanTurn = true;
 		startGame();
 	}
 
 	public void move(int sourceRow, int sourceColumn, int targetRow, int targetColumn) {
+		if (!humanTurn){
+			return;
+		}
+		
 		Point source = Point.getInstance(sourceColumn, sourceRow);
 		Point target = Point.getInstance(targetColumn, targetRow);
 		Movement move = new Movement(source, target);
 		this.game.move(move);
+		humanTurn = false;
 	}
 
 	public void onTileUpdate(int row, int column, char player) {
@@ -38,6 +46,25 @@ public class GameController{
 		container.setGame(game.getBoardHeight(), game.getBoardWidth());
 		container.setGameVisible();
 		game.start(this);
+		
+		boolean win = false;
+		MinMaxTree ai; 
+	
+		while (!win){
+			if (humanTurn) {
+				
+				try {
+				    Thread.sleep(1000);
+				} catch(InterruptedException ex) {
+				    Thread.currentThread().interrupt();
+				}
+				
+			} else {
+				ai = new MinMaxTree(game.getStrategy(), game.getBoard(), 4, game.getPlayers());
+				this.game.move(ai.getBestMove());
+				humanTurn = true;
+			}
+		}
 	}
 
 	public void onWin() {
