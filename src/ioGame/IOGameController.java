9330aa212c53237b40f3deca41b7b5422b8b4be0
Kevin J. Hanna <kevinjackhanna@gmail.com>
2012-10-30 13:56:18 -0300

    Add Options class

--- src/ioGame/IOGameController.java
+++ src/ioGame/IOGameController.java
@@ -1,9 +1,9 @@
 package ioGame;
 
-
 import game.Computer;
 import game.Game;
 import game.Human;
+import game.Movement;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -14,29 +14,51 @@ import java.io.IOException;
 import ai.LevelMinimax;
 import ai.Minimax;
 
-
 public class IOGameController {
 	File file;
 	Game game;
 	
-	public IOGameController(String fileName, String player){
+	public IOGameController(String fileName, String playerNumber){
 		char[] charBoard = openCharBoard(fileName);
-		Human human = new Human();
-		Computer computer = new Computer();
+		
+		if (playerNumber.equals("2")){
+			invertPlayers(charBoard);
+		}
 		
 		this.game = new Game(charBoard);
+		
 		System.out.println(this.game.getBoard());
 		//TODO: delete magic number
 		Minimax ai = new LevelMinimax(game.getStrategy(), game.getBoard(), 4, game.getHuman(), game.getComputer());
 		System.out.println(ai.getBestMove());
+		
+		
+		Movement move = ai.getBestMove();
+		
+		if (move == null){
+			System.out.println("PASS");
+		} else {
+			System.out.println(ai.getBestMove());
+		}
 			
+	}
+
+	
+	private void invertPlayers(char[] charBoard){
+		for (int i = 0; i < charBoard.length; i++){
+			if (charBoard[i] == '1'){
+				charBoard[i] = '2';
+			} else if (charBoard[i] == '2') {
+				charBoard[i] = '1';
+			}
+		}
 	}
 	
 	private char[] openCharBoard(String path){
 		try {
 			BufferedReader buffer = new BufferedReader(new FileReader(path));
 			
-			char[] file = new char[64];
+			char[] file = new char[game.getBoardHeight() * game.getBoardWidth()];
 			
 			buffer.read(file);
 			buffer.close();
---
+++ src/ioGame/Options.java
@@ -0,0 +1,56 @@
+package ioGame;
+
+public class Options {
+	private String[] args;
+	private String mode;
+	private String fileName;
+	private String player;
+	
+	public Options(String[] args){
+		this.mode = args[0].substring(1);
+		this.args = args;
+	}
+	
+	
+	public String getFileName(){
+		return fileName;
+	}
+	
+	public String getPlayerNumber(){
+		return player;
+	}
+	
+	
+	public boolean valid(){
+		if (!consoleMode() && !visualMode()){
+			return false;
+		}
+		
+		if (consoleMode()){
+			if (this.args.length < 4){
+				return false;
+			}
+			
+			this.fileName = args[1];
+			
+			if (args[3].equals("1")) {
+				this.player = "1";
+			} else if (args[3].equals("2")){
+				this.player = "2";
+			} else {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public boolean visualMode(){
+		return mode.equals("visual");
+	}
+	
+	public boolean consoleMode(){
+		return mode.equals("file");
+	}
+
+}
