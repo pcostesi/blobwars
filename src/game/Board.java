9b7f0b99bdf21b6d40253a558873e936f309c5b4
Kevin J. Hanna <kevinjackhanna@gmail.com>
2012-11-01 23:13:28 -0300

    Add board javadoc

--- src/game/Board.java
+++ src/game/Board.java
@@ -8,14 +8,27 @@ import structures.Pair;
 import structures.Point;
 import util.Chain;
 
+/**
+ * The Board represents a squared game board. In reality, it is implemented by a large array of references to each Player.
+ */
 public class Board implements Cloneable {
+	
 	protected static final int SIZE = 8;
+	
 	private Strategy strategy;
 	
 	Player[] owner;
+	
 	private Player player1;
 	private Player player2;
 	
+	/**
+	 * Instantiates a new board.
+	 *
+	 * @param strategy the strategy
+	 * @param pl1 the pl1
+	 * @param pl2 the pl2
+	 */
 	public Board(Strategy strategy, Player pl1, Player pl2){
 		this.strategy = strategy;
 		this.owner = new Player[SIZE * SIZE];
@@ -23,11 +36,24 @@ public class Board implements Cloneable {
 		player2 = pl2;
 	}
 	
+	/**
+	 * Instantiates a new board with a given array of chars 1 for player 1, 2 for player 2
+	 *
+	 * @param strategy the strategy
+	 * @param pl1 the pl1
+	 * @param pl2 the pl2
+	 * @param charBoard the char board
+	 */
 	public Board(Strategy strategy, Player pl1, Player pl2, char[] charBoard){
 		this(strategy, pl1, pl2);
 		convertCharsToPlayers(charBoard);
 	}
 	
+	/**
+	 * Convert an array of chars to the board players.
+	 *
+	 * @param charBoard the char board
+	 */
 	private void convertCharsToPlayers(char[] charBoard){
 		for (int i = 0; i < SIZE * SIZE; i++){
 			if (charBoard[i] == '1'){
@@ -39,6 +65,12 @@ public class Board implements Cloneable {
 		
 	}
 	
+	/**
+	 * Count tiles for a given player.
+	 *
+	 * @param p the player
+	 * @return the number of tiles
+	 */
 	public int countTilesForPlayer(Player p){
 		int base = 0;
 		for (Player cur : owner){
@@ -49,6 +81,12 @@ public class Board implements Cloneable {
 		return base;
 	}
 	
+	/**
+	 * Gradient for player.
+	 *
+	 * @param p the p
+	 * @return the int
+	 */
 	public int gradientForPlayer(Player p){
 		int base = 0;
 		for (Player cur : owner){
@@ -62,6 +100,11 @@ public class Board implements Cloneable {
 		return base;
 	}
 	
+	/**
+	 * Count all the used, not empty tiles.
+	 *
+	 * @return the count
+	 */
 	public int countTiles(){
 		int tiles = 0;
 		for (int i = 0; i < SIZE * SIZE; i++){
@@ -72,43 +115,86 @@ public class Board implements Cloneable {
 		return tiles;
 	}
 	
+	/**
+	 * Instantiates a new board from an other board. Note that it only creates a copy of the tiles.
+	 *
+	 * @param other the other
+	 */
 	public Board(Board other){
-		//this.tiles = Arrays.copyOf(other.tiles, SIZE * SIZE);
 		this.owner = Arrays.copyOf(other.owner, SIZE * SIZE);
 		this.player1 = other.player1;
 		this.player2 = other.player2;
 		this.strategy = other.strategy;
-		
 	}
 	
 	
+	/**
+	 * Gets the board width.
+	 *
+	 * @return the width
+	 */
 	public int getWidth(){
 		return SIZE;
 	}
 	
+	/**
+	 * Gets the board height.
+	 *
+	 * @return the height
+	 */
 	public int getHeight(){
 		return SIZE;
 	}
 	
-	//ATTENTION only method that modifies board
+	/**
+	 * Sets a tile.
+	 *
+	 * @param target the location
+	 * @param p the player
+	 */
 	public void setTile(Point target, Player p){
 		owner[pointToIndex(target)] = p;
 	}
 	
+	/**
+	 * Deletes a tile.
+	 *
+	 * @param target the location
+	 */
 	public void deleteTile(Point target){
 		owner[pointToIndex(target)] = null;
 	}
 	
+	/**
+	 * Puts blob. Similar to setTile, but returns the board.
+	 *
+	 * @param player the player
+	 * @param target the target
+	 * @return the board
+	 */
 	public Board putBlob(Player player, Point target){
 		setTile(target, player);
 		return this;
 	}
 	
-	 public Board deleteBlob(Player player, Point target){
+	 /**
+ 	 * Delete blob. Similar to deleteTile, but requires a Player
+ 	 *
+ 	 * @param player the player
+ 	 * @param target the target
+ 	 * @return the board
+ 	 */
+ 	public Board deleteBlob(Player player, Point target){
 		deleteTile(target);
 		return this;
 	}
 	 
+	/**
+	 * Gets the tile (char) in a given location.
+	 *
+	 * @param source the location
+	 * @return the tile, c for computer, h for human
+	 */
 	public char getTile(Point source){
 		Player p = getTileOwner(source);
 		if (p == null){
@@ -117,11 +203,23 @@ public class Board implements Cloneable {
 		return p.toTile();
 	}
 	
+	/**
+	 * Gets the tile owner.
+	 *
+	 * @param source the source
+	 * @return the tile owner
+	 */
 	public Player getTileOwner(Point source){
 		return owner[pointToIndex(source)];
 	}
 	
 	
+	/**
+	 * Generate children.
+	 *
+	 * @param player the player
+	 * @return the iterable
+	 */
 	public Iterable<Pair<Board, Movement>> generateChildren(Player player){
 		List<Iterable<Pair<Board, Movement>>> children = new ArrayList<Iterable<Pair<Board, Movement>>>(SIZE * SIZE);
 		for (int i = 0; i < SIZE; i++){
@@ -138,6 +236,7 @@ public class Board implements Cloneable {
 	}
 	
 	
+
 	public String toString(){
 		StringBuilder result = new StringBuilder();
 
@@ -162,18 +261,37 @@ public class Board implements Cloneable {
 		return result.toString();
 	}
 
+	/**
+	 * Point to index. Used to convert a Point into a number to retrieve a char for the owner array.
+	 *
+	 * @param p the point
+	 * @return the int
+	 */
 	private int pointToIndex(Point p){
 		return (p.getX() + p.getY() * SIZE);
 	}
 	
+
 	public Object clone(){
 		return new Board(this);
 	}
 	
+	/**
+	 * Checks for available move for a certain Point
+	 *
+	 * @param source the source
+	 * @return true, if successful
+	 */
 	public boolean hasAvailableMove(Point source){
 		return strategy.hasAvailableMove(this, source);
 	}
 
+	/**
+	 * Checks for available moves for a player, looks for every tile it has.
+	 *
+	 * @param player the player
+	 * @return true, if successful
+	 */
 	public boolean hasAvailableMoves(Player player){
 		for (int i = 0; i < SIZE; i++){
 			for (int j = 0; j < SIZE; j++){
@@ -188,6 +306,12 @@ public class Board implements Cloneable {
 		return false;
 	}
 	
+	/**
+	 * Count available moves for a given player.
+	 *
+	 * @param player the player
+	 * @return the int
+	 */
 	public int countAvailableMoves(Player player){
 		int itr = 0;
 		for (int i = 0; i < SIZE; i++){
